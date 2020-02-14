7b82ae06f4e60b906a4233828b9543c0f20179f7
Aaron Sullivan <aasullivan1618@gmail.com>
2020-02-14 12:48:19 -0800

    Add DB_PORT option to knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,10 @@
 module.exports = {
   client: 'mysql',
   connection: {
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DB,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB,
+    port: process.env.DB_PORT
   }
 }
